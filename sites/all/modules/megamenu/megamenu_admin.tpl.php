9f9d4f28f8c75d46fe8382d9fb10b2fd6774a1bf
IshaDakota <adam@marchinglizard.net>
2011-05-31 14:44:09 -0500

    adding all modules for production site

---
+++ sites/all/modules/megamenu/megamenu_admin.tpl.php
@@ -0,0 +1,27 @@
+<?php print drupal_render($form['title']); ?>
+<?php print drupal_render($form['markup']); ?>
+
+<table>
+  <thead>
+    <tr>
+      <th>Menu</th>
+      <th>Options</th>
+      <th>Enable Mega Menu Block</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($menu as $m): ?>
+      <tr>
+        <td><h3><?php print $m['details']['title']; ?></h3></td>
+        <td><?php print l('configure mega','admin/config/megamenu/settings/' . $m['details']['menu_name']); ?></td>
+        <td>
+          <?php print drupal_render($form['enabled'][$m['details']['menu_name']]); ?>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+<?php print drupal_render($form['advanced']); ?>
+<div><?php print drupal_render($form['save_configuration']); ?></div>
+<?php print drupal_render_children($form); // Print the closing form elements (ID and hidden validation tags) ?>
+
